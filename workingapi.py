38e4d69adc19aac0a14c0ef7a546bf1874298232
rigo0523 <limon1285@gmail.com>
2021-12-01 23:37:44 -0600

    test params endpoint

--- workingapi.py
+++ workingapi.py
@@ -2,6 +2,25 @@ from fastapi import FastAPI
 
 server = FastAPI()
 
+inventory = {
+    1: {
+        "name": "Milk",
+         "price": 3.99,
+        "brand": "Regular"
+    },
+    2:   {
+        "name": "mountain"
+    }
+}
+
 @server.get("/")
 def home(): 
     return {"Data": "testing"}
+
+@server.get("/about")
+def about(): 
+    return {"Data": "about"}
+
+@server.get("/get-item/{id}")
+def get_item(id: int):
+    return inventory[id]
