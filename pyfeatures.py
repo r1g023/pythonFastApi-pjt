e822a35be9a1c6e6acbc237ded417f6c6ad3617e
rigo0523 <limon1285@gmail.com>
2021-12-02 23:23:25 -0600

    eval real time console tests

---
+++ pyfeatures.py
@@ -0,0 +1,8 @@
+import json
+
+items = json.loads('[{"id" : 1, "text": "Item 1"}, {"id" : 2, "text": "Item 2"}]')
+
+for item in items:
+    print(item["text"])
+    print(item["id"] + 1)
+    print("hello")
