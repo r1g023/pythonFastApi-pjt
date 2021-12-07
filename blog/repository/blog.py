4771ae30ea9eb1d8c3b8ef7d85f50158c23243c9
rigo0523 <limon1285@gmail.com>
2021-12-06 23:42:28 -0600

    refactored paths onto repository/blog.py

---
+++ blog/repository/blog.py
@@ -0,0 +1,46 @@
+from fastapi import HTTPException, status
+from sqlalchemy.orm import Session
+from .. import models, schemas
+
+# GET /api/blogs
+def get_all(db: Session):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+# GET /api/blogs/:Id
+def getById(id: int, db: Session):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    return blog
+
+
+# POST /api/blogs
+def create(blog_request: schemas.Blog, db: Session):
+    new_blog = models.Blog(title=blog_request.title, body=blog_request.body, user_id=blog_request.user_id)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+# DELETE /api/blogs/:id
+def deleteBlog(id: int, db: Session):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return "deleted"
+
+
+# UPDATE /api/blogs/:id
+def updateBlog(id: int, request_blog: schemas.Blog, db: Session):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    # or update by dict(request_blog) or request_blog.dict()
+    blog.update(dict(request_blog))
+    db.commit()
+    return "Updated Blog"
--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from .. import schemas, models, database
 from typing import List
 from sqlalchemy.orm import Session
+from ..repository import blog
 
 router = APIRouter()
 
@@ -10,49 +11,28 @@ router = APIRouter(prefix="/api/blog", tags=["Blogs"])
 # GET blogs
 @router.get("/", response_model=List[schemas.ShowBlog])
 def getBlogs(db: Session = Depends(database.get_db)):  # database instance
-    blogs = db.query(models.Blog).all()
-
-    return blogs
+    return blog.get_all(db)  # GET FROM REPOSITORY folder
 
 
 # GET /api/blog/id
 @router.get("/{id}", response_model=schemas.ShowBlog)
 def blogID(id: int, db: Session = Depends(database.get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
-    return blog
+    return blog.getById(id, db)
 
 
 # POST /api/blogs
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def create(blog_request: schemas.Blog, db: Session = Depends(database.get_db)):
-    new_blog = models.Blog(title=blog_request.title, body=blog_request.body, user_id=blog_request.user_id)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+    return blog.create(blog_request, db)  # GET FROM REPOSITORY folder
 
 
 # DELETE /api/blog/id
 @router.delete("/{id}")
 def destroy(id: int, db: Session = Depends(database.get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
-    blog.delete(synchronize_session=False)
-    db.commit()
-    return "deleted"
+    return blog.deleteBlog(id, db)
 
 
 # UPDATE /api/blog/id
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
-def updateBlog(id, request_blog: schemas.Blog, db: Session = Depends(database.get_db)):
-
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
-    # or update by dict(request_blog) or request_blog.dict()
-    blog.update(dict(request_blog))
-    db.commit()
-    return "Updated Blog"
+def updateBlog(id: int, request_blog: schemas.Blog, db: Session = Depends(database.get_db)):
+    return blog.updateBlog(id, request_blog, db)
